7eaa22ba1c0a65735f43408cf09f07bce246b3d8
AlinaTaoRao <raotao77@outlook.com>
2022-04-19 09:51:19 +0200

    finish app react version

---
+++ netflix-login/src/components/sub-header/SubHeader.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export const SubHeader = () => {
+  return (
+    <div className="sub-header">
+      <a className="header-link" href="#">
+        New to Netflix? <span>Sign up Now</span>
+      </a>
+    </div>
+  );
+};
