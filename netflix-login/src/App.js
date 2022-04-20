c974d6462113b5949601458af6905e7e2db62d93
AlinaTaoRao <raotao77@outlook.com>
2022-04-20 11:22:22 +0200

    fix header background issue

--- netflix-login/src/App.js
+++ netflix-login/src/App.js
@@ -1,3 +1,4 @@
+// import "./App.css";
 import "./App.css";
 import { LoginFormContainer } from "./components/LoginFormContainer.jsx";
 
--- netflix-login/src/components/sign-in-form/SignInForm.jsx
+++ netflix-login/src/components/sign-in-form/SignInForm.jsx
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./signin.css";
 
 export const SignInForm = () => {
   return (
